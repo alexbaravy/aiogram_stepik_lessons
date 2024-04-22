85c915cec4f92524369f1351ed1f3f900019ec2d
Alex <alexbaravy@gmail.com>
2024-04-22 19:39:32 +0300

    start book reader bot

---
+++ bot_example/book_reader/main.py
@@ -0,0 +1,75 @@
+﻿import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from config_data.config import Config, load_config
+# Импортируем роутеры
+# ...
+# Импортируем миддлвари
+# ...
+# Импортируем вспомогательные функции для создания нужных объектов
+# ...
+from keyboards.main_menu import set_main_menu
+
+# Инициализируем логгер
+logger = logging.getLogger(__name__)
+
+
+# Функция конфигурирования и запуска бота
+async def main():
+    # Конфигурируем логирование
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s')
+
+    # Выводим в консоль информацию о начале запуска бота
+    logger.info('Starting bot')
+
+    # Загружаем конфиг в переменную config
+    config: Config = load_config()
+
+    # Инициализируем объект хранилища
+    storage = ...
+
+    # Инициализируем бот и диспетчер
+    bot = Bot(
+        token=config.tg_bot.token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+    dp = Dispatcher(storage=storage)
+
+    # Инициализируем другие объекты (пул соединений с БД, кеш и т.п.)
+    # ...
+
+    # Помещаем нужные объекты в workflow_data диспетчера
+    dp.workflow_data.update(...)
+
+    # Настраиваем главное меню бота
+    await set_main_menu(bot)
+
+    # Регистриуем роутеры
+    logger.info('Подключаем роутеры')
+    # ...
+
+    # Регистрируем миддлвари
+    logger.info('Подключаем миддлвари')
+    # ...
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+asyncio.run(main())
+
+# Выводим значения полей экземпляра класса Config на печать,
+# чтобы убедиться, что все данные, получаемые из переменных окружения, доступны
+print('BOT_TOKEN:', config.tg_bot.token)
+print('ADMIN_IDS:', config.tg_bot.admin_ids)
+print()
+print('DATABASE:', config.db.database)
+print('DB_HOST:', config.db.db_host)
+print('DB_USER:', config.db.db_user)
+print('DB_PASSWORD:', config.db.db_password)
