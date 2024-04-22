84e52f84c3baf44dfc2478deec172fd09d44f7e8
Alex <alexbaravy@gmail.com>
2024-04-22 19:47:13 +0300

    added config

--- bot_example/book_reader/config_data/config.py
+++ bot_example/book_reader/config_data/config.py
@@ -3,14 +3,6 @@ from environs import Env
 
 
 @dataclass
-class DatabaseConfig:
-    database: str  # Название базы данных
-    db_host: str  # URL-адрес базы данных
-    db_user: str  # Username пользователя базы данных
-    db_password: str  # Пароль к базе данных
-
-
-@dataclass
 class TgBotConfig:
     token: str  # Токен для доступа к телеграм-боту
     admin_ids: list[int]  # Список id администраторов бота
@@ -19,9 +11,10 @@ class TgBotConfig:
 @dataclass
 class Config:
     tg_bot: TgBotConfig
-    db: DatabaseConfig
 
 
+# Создаем функцию, которая будет читать файл .env и возвращать
+# экземпляр класса Config с заполненными полями token и admin_ids
 def load_config(path: str | None = None) -> Config:
     env: Env = Env()
     env.read_env(path)
@@ -30,11 +23,5 @@ def load_config(path: str | None = None) -> Config:
         tg_bot=TgBotConfig(
             token=env('BOT_TOKEN'),
             admin_ids=list(map(int, env.list('ADMIN_IDS')))
-        ),
-        db=DatabaseConfig(
-            database=env('DATABASE'),
-            db_host=env('DB_HOST'),
-            db_user=env('DB_USER'),
-            db_password=env('DB_PASSWORD')
         )
     )
